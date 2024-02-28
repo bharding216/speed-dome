c2e32624f8eb44a0e5c7d3e65c9d08c3694dab88
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2024-02-28 11:57:42 -0600

    Add Stripe payment integration and shipping address form

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -6,27 +6,91 @@
 <script>
 	import { cart, clearCart } from '../../stores/cartStore';
 	import { onMount } from 'svelte';
+	// import { Stripe } from 'stripe';
+	import { loadStripe } from '@stripe/stripe-js';
   
+	let stripe;
+	let elements;
+	let options;
+	let message = '';
+
 	let cartItems = [];
 
 	function handleClearCart() {
 		clearCart();
 	}
 
-	onMount(() => {
+	onMount(async () => {
+		// Load Stripe instance
+		stripe = await loadStripe('pk_test_51OnpWBDDaS82tif1vkx6CfoMEN20PjVL1HkoGVKBjtBbNC9DNgegnyO5unw9pglPOwyeLLIl79NTd1gQqFVgNZ4q00R0hFFrAS');
+		options = { mode: 'shipping' };
+
+		const { clientSecret, message } = await fetch('https://hzlw7ocdo6owj22xn5vqnzjqs40waper.lambda-url.us-east-2.on.aws/api/create-payment-intent').then((res) => res.json());
+
+		console.log('clientSecret:', clientSecret);
+		console.log('message:', message);
+
+		elements = stripe.elements({ clientSecret });
+		const paymentElement = elements.create('payment');
+		paymentElement.mount('#payment-element');
+
+		const addressElement = elements.create('address', options);
+		addressElement.mount('#address-element');
+
 		// Subscribe to changes in the cart store
 		const unsubscribe = cart.subscribe((items) => {
 			cartItems = items;
 			console.log('cartItems:', cartItems);
 		});
 
-		// Unsubscribe when the component is destroyed
-		return unsubscribe;
+		return () => {
+			// Clean up when the component is destroyed
+			paymentElement.destroy();
+
+			// Unsubscribe from the store
+			unsubscribe();
+		};
 	});
+
+	const handleSubmit = async () => {
+		try {
+			const { error } = await stripe.confirmPayment({
+				elements,
+				confirmParams: {
+					shipping: {
+						address: {
+							city: 'San Francisco',
+							country: 'US',
+							line1: '1234 Main St',
+							postal_code: '94111',
+							state: 'CA'
+						},
+						name: 'Jenny Rosen',
+						phone: '4158675309'
+					},
+					return_url: `${window.location.origin}/cart`
+				}
+			});
+
+			if (error) {
+				console.log('Payment failed');
+				console.log('Error type:', error.type);
+				console.log('Error message:', error.message);
+			} else {
+				message = 'Payment successful';
+			}
+		} catch (error) {
+			console.log('Error confirming payment', error);
+		}
+	}
+
+
 </script>
 
-<div class="row text-center">
-	<div class="col-12 col-sm-6 mb-5 mb-sm-0 d-flex flex-column justify-content-center">
+<p>{message}</p>
+
+<div class="row">
+	<div class="col-12 col-sm-6">
 		<div class="row pb-3 pt-5">
 			<div class="col-12">
 				<h2>The Cart Page</h2>
@@ -60,11 +124,33 @@
 		</div>
 	</div>
 
+	<div class="col-12 col-sm-6">
+		<div class="pb-4 card">
+			<h3>Shipping Address</h3>
+			<div id="address-element"></div>
+		</div>
+
+		<div class="card">
+			<h3>Payment</h3>
+			<div id="payment-element"></div>
+		</div>
+
+		<button id="submit" type="button" class="btn btn-success" on:click={handleSubmit}>
+			Place your order
+		</button>
+	</div>
+
 
 </div>
 
 
 <style>
-
+    .card {
+        border: 1px solid #ccc;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+        margin-bottom: 20px;
+        border-radius: 4px;
+    }
 
 </style>
